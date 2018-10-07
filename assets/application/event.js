b9860636d3e0b80a68bb29438a2876eeb423d660
HurSpar <brianrice2382@gmail.com>
2018-10-07 17:37:23 -0500

    update from saturday

--- assets/application/event.js
+++ assets/application/event.js
@@ -1,35 +1,55 @@
 var config = {
-    apiKey: “AIzaSyAno_Lj1JInIRz89IGUAMYqaElUD-zKBIE”,
-    authDomain: “api-project-1-1538239416627.firebaseapp.com”,
-    databaseURL: “https://api-project-1-1538239416627.firebaseio.com“,
-    projectId: “api-project-1-1538239416627”,
-    storageBucket: “”,
-    messagingSenderId: “205626442574”
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyAno_Lj1JInIRz89IGUAMYqaElUD-zKBIE",
+  authDomain: "api-project-1-1538239416627.firebaseapp.com",
+  databaseURL: "https://api-project-1-1538239416627.firebaseio.com",
+  projectId: "api-project-1-1538239416627",
+  storageBucket: "api-project-1-1538239416627.appspot.com",
+  messagingSenderId: "205626442574"
+};
+firebase.initializeApp(config);
 
   var database = firebase.database();
 //Button event to initialize search
   $("#search-event").on("click", function (event){
 
-
+//sets variables  to user input 
     var eventFind = $("#find-input").val().trim();
     var eventDate = $("#date-input").val().trim();
     var eventZip = $("#zipcode-input").val().trim();
 
+    console.log(eventFind);
 
+//pushes data to firebase
     var newEvent = {
         keyword: eventFind,
         date: eventDate,
         zipcode: eventZip
     };
 
-    database.ref().push(newEvent);
+  database.ref().push(newEvent);
 
-    console.log(newEvent.keyword);
+  console.log(newEvent.keyword);
   console.log(newEvent.date);
   console.log(newEvent.zipcode);
+
+  function displayeventInfo() {
+    var queryURL ="http://api.eventful.com/rest/events/search?...&keywords=books&location=San+Diego&date=Future";
+     
+      $.ajax({
+      url: queryURL,
+      crossDomain: true,
+      method: "GET"
+    }).then(function(response) {
+      var eventfuls = response.events.title;
+      console.log(eventfuls);
+
+    }
+    )};
+
+displayeventInfo();
+  
 });
+
 
 
 database.ref().on("child_added", function( childSnapshot, prevChildKey){
@@ -41,6 +61,9 @@ var eventDate = childSnapshot.val().date;
 var eventZip = childSnapshot.val().zipcode;
 
 console.log(eventFind);
-console.log(eventFind);
+console.log(eventDate);
+console.log(eventZip);
 
 })
+
+
