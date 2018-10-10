d628a0ac5e3ddb1480e13c9c8d8f48a13b2deb14
HurSpar <brianrice2382@gmail.com>
2018-10-09 20:44:23 -0500

    working

--- assets/application/event.js
+++ assets/application/event.js
@@ -1,6 +1,6 @@
-var eventFind = "";
-    var eventDate = "";
-    var eventZip = "";
+eventFind = "";
+     eventDate = "";
+     eventZip = "";
 
 var config = {
   apiKey: "AIzaSyAno_Lj1JInIRz89IGUAMYqaElUD-zKBIE",
@@ -13,64 +13,68 @@ var config = {
 firebase.initializeApp(config);
 
   var database = firebase.database();
-  
-
 //Button event to initialize search
   $("#search-event").on("click", function (event){
+    event.preventDefault();
 
 //sets variables  to user input 
      eventFind = $("#find-input").val().trim();
      eventDate = $("#date-input").val().trim();
      eventZip = $("#zipcode-input").val().trim();
+     console.log(eventZip);
 
-        
     
 
 //pushes data to firebase
-    var newEvent = {
-        keyword: eventFind,
-        date: eventDate,
-        zipcode: eventZip
-    };
+  //   var newEvent = {
+  //       keyword: eventFind,
+  //       date: eventDate,
+  //       zipcode: eventZip
+  //   };
 
-  database.ref().push(newEvent);
+  // database.ref().push(newEvent);
 
-
+  // console.log(newEvent.keyword);
+  // console.log(newEvent.date);
+  // console.log(newEvent.zipcode);
+database.ref().set({
+  keyword: eventFind,
+  zipcode: eventZip
+});
   
 
+console.log(eventZip);
+
+database.ref().on("value", function(snapshot){
+
+  eventFind = snapshot.val().keyword;
+  eventZip = snapshot.val().zipcode;
+ });
+console.log(eventZip);
+console.log(eventFind);
+
+var oArgs = {
+ app_key: "HHnGr5bXXXFGjzZW",
+ location: eventZip,
+ q: eventFind,
+ "date": "Future",
+ "include": "tags,categories",
+ page_size: 5,
+ sort_order: "popularity",
+};
+EVDB.API.call("/events/search", oArgs, function(oData) {
+ console.log(oData);
+ console.log(oData.events.event[0].image);
+ var picURL = oData.events.event[0].image.medium.url;
+ var picTure = $("<img>").attr("src", picURL);
+ var titleURL = oData.events.event[0].title;
+$("#event-data").append(picTure);
+$("#event-data").append(titleURL);
+ });
+  
 });
 
 
-var oArgs = {
-  app_key: "HHnGr5bXXXFGjzZW",
-  location: "San Diego",
-  // q: eventfind,
-  "date": "Future",
-  "include": "tags,categories",
-  page_size: 5,
-  sort_order: "popularity",
-};
-EVDB.API.call("/events/search", oArgs, function(oData) {
-  console.log(oData);
-  console.log(oData.events.event[0].image);
-  var picURL = oData.events.event[0].image.medium.url;
-  var picTure = $("<img>").attr("src", picURL);
-  var titleURL = oData.events.event[0].title;
-$("#event-data").append(picTure);
-$("#event-data").append(titleURL);
-  });
 
-
-// database.ref().on("child_added", function( childSnapshot, prevChildKey){
-
-// var eventFind = childSnapshot.val().keyword;
-// var eventDate = childSnapshot.val().date;
-// var eventZip = childSnapshot.val().zipcode;
-
-
-
-// })
-
-  
 
 
